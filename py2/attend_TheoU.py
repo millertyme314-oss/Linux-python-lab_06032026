978bf0bbf3c5d277efdac5618efff40d4f17bfc7
Zeek Millz <228266305+millertyme314-oss@users.noreply.github.com>
2026-06-13 17:28:29 -0400

    evidence of work-final

--- py2/attend_TheoU.py
+++ py2/attend_TheoU.py
@@ -4,13 +4,12 @@ In_Class = int(input("How many classes attended per week? "))
 Replay = int(input("How many times in Replay-Gang? "))
 
 
-def Theo_Univ():
-    print("Theo University is in session... ")
+Theo_Univ = "Theo University is in session..."
 
 
 if In_Class >= 1 and Replay >= 1:
     print(Theo_Univ +", Welcome to the Blue Ocean")
 elif In_Class < 1 and Replay >= 1:
-    print(Theo_Univ, "Gynocracy awaits...if you don't shape up!")
+    print(Theo_Univ,"Gynocracy awaits; if you don't shape up!")
 else: 
-    print(Theo_Univ, "Piss Off...Keisha awaits in Red Ocean!!")
+    print(Theo_Univ,"Piss Off; Keisha awaits in Red Ocean!!")
